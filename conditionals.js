b55b512f6a6f53d4682c9815e128f06c90b758b7
Yaghya_Abdul_GIT <yaghyaabdul1006@gmail.com>
2026-05-25 09:51:12 +0200

    Add files via upload

---
+++ conditionals.js
@@ -0,0 +1,13 @@
+const score = 100;
+const passingGrade = 60;
+
+if (score >= passingGrade) {
+    console.log("Passed");
+}
+else {
+    console.log("Failed");
+}
+
+if (score === 100){
+    console.log("Perfect Score!!!");
+}
